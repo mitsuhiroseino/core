388770883dc67852a2d39e4748861c2f5bd12be6
SURFACEPRO9\mitsu <mitsuhiro.seino@gmail.com>
2024-06-16 13:17:33 +0900

    overrideInstance

--- src/utils/object/index.ts
+++ src/utils/object/index.ts
@@ -3,6 +3,7 @@ export { default as flatten } from './flatten';
 export { default as format } from './format';
 export { default as group } from './group';
 export { default as hasOwnProperty } from './hasOwnProperty';
+export { default as overrideInstance } from './overrideInstance';
 export { default as parse } from './parse';
 export type * from './types';
 export { default as update } from './update';
